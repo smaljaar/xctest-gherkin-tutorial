21697695384e7436ecea4000875ef46be3c293b5
Samuël Maljaars <smaljaars@smaljaars.local>
2016-08-22 12:08:24 +0200

    implementation SendPaymentVC and ConfirmationVC

--- EspressoBank/ConfirmationViewController.swift
+++ EspressoBank/ConfirmationViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ConfirmationViewController: UIViewController {
 
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var iban: UILabel!
+    @IBOutlet weak var amount: UILabel!
+    @IBOutlet weak var paymentDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        name.text = history.payments.last?.name
+        iban.text = history.payments.last?.iban
+        amount.text = history.payments.last?.amount.description
+        paymentDescription.text = history.payments.last?.paymentDescription
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,7 +33,7 @@ class ConfirmationViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -30,6 +41,5 @@ class ConfirmationViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
--- EspressoBank/SendPaymentViewController.swift
+++ EspressoBank/SendPaymentViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
+struct Payment {
+    var name: String
+    var iban: String
+    var amount: NSNumber
+    var paymentDescription: String
+}
+
+let history = PaymentHistory()
+
+class PaymentHistory {
+    var payments = [Payment]()
+}
+
 class SendPaymentViewController: UIViewController {
 
+    var payment = Payment(name: "", iban: "", amount: 0.0, paymentDescription: "")
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var iban: UITextField!
+    @IBOutlet weak var amount: UITextField!
+    @IBOutlet weak var paymentDescription: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        name.delegate = self
+        iban.delegate = self
+        amount.delegate = self
+        paymentDescription.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +45,41 @@ class SendPaymentViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        history.payments.append(payment)
 
+    }
+}
+
+extension SendPaymentViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        guard let value = textField.text else {
+            return
+        }
+        
+        switch textField {
+        case name:
+            payment.name = value
+        case iban:
+            payment.iban = value
+        case amount:
+            if let myValue = Int(value){
+                payment.amount = NSNumber(integer: myValue)
+                
+            }
+        case paymentDescription:
+            payment.paymentDescription = value
+        default:
+            break;
+        }
+        
+    }
+    
 }
