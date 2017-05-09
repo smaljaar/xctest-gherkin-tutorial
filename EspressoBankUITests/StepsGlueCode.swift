67ec5edc1831d07c97410ec1ec1660b9842d60da
Samuël Maljaars <smaljaars@smaljaars.local>
2017-05-09 20:55:51 +0200

    Fixed test to work on iPad

--- EspressoBankUITests/StepsGlueCode.swift
+++ EspressoBankUITests/StepsGlueCode.swift
@@ -19,19 +19,22 @@ class Steps : StepDefiner {
         }
         step("I make a transfer") {
             app.descendants(matching:.any)["Transfer"].tap()
-            app.descendants(matching:.any)["Name"].tap()
-            app.descendants(matching:.any)["Name"].typeText("Yougert")
-            app.descendants(matching:.any)["IBAN"].tap()
-            app.descendants(matching:.any)["IBAN"].typeText("Melk")
-            app.descendants(matching:.any)["Description"].tap()
-            app.descendants(matching:.any)["Description"].typeText("Melk")
+            
             app.descendants(matching:.any)["Amount"].tap()
-            app.descendants(matching:.any)["Amount"].typeText("13.37")
+            app.descendants(matching:.any)["Amount"].typeText("13.37\n")
+            
+            app.descendants(matching:.any)["Name"].tap()
+            app.descendants(matching:.any)["Name"].typeText("Yougert\n")
+            
+            
+            
+            app.descendants(matching:.any)["IBAN"].tap()
+            app.descendants(matching:.any)["IBAN"].typeText("Melk\n")
             app.descendants(matching:.any)["Send Payment"].tap()
             app.descendants(matching:.any)["Confirm Payment"].tap()
         }
         step("I see a transaction") {
-            let transaction = app.descendants(matching:.any)["Melk, Melk, - € 13.37"]
+            let transaction = app.descendants(matching:.any)["- € 13.37"]
             XCTAssertTrue(transaction.isHittable)
         }
         
--- EspressoBankUITests/features/answers.feature
+++
@@ -1,4 +0,0 @@
-Feature: Transfer money
-
-Scenario: Test
-
