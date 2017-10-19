c989ecac4601689addeaa3084cab5fc633bf89be
Samuël Maljaars <samuel.maljaars@clockwork.nl>
2017-10-19 16:14:51 +0200

    use firstMatch api so test runs faster

--- EspressoBankUITests/StepsGlueCode.swift
+++ EspressoBankUITests/StepsGlueCode.swift
@@ -18,21 +18,21 @@ class Steps : StepDefiner {
             app.launch()
         }
         step("I make a transfer") {
-            app.descendants(matching:.any)["Transfer"].tap()
+            app.descendants(matching:.any)["Transfer"].firstMatch.tap()
             
-            app.descendants(matching:.any)["Amount"].tap()
-            app.descendants(matching:.any)["Amount"].typeText("13.37\n")
-            app.descendants(matching:.any)["Name"].tap()
-            app.descendants(matching:.any)["Name"].typeText("Yoghurt\n")
-            app.descendants(matching:.any)["IBAN"].tap()
-            app.descendants(matching:.any)["IBAN"].typeText("NL69INGB0123456789\n")
+            app.descendants(matching:.any)["Amount"].firstMatch.tap()
+            app.descendants(matching:.any)["Amount"].firstMatch.typeText("13.37\n")
+            app.descendants(matching:.any)["Name"].firstMatch.tap()
+            app.descendants(matching:.any)["Name"].firstMatch.typeText("Yoghurt\n")
+            app.descendants(matching:.any)["IBAN"].firstMatch.tap()
+            app.descendants(matching:.any)["IBAN"].firstMatch.typeText("NL69INGB0123456789\n")
             
-            app.descendants(matching:.any)["Send Payment"].tap()
+            app.descendants(matching:.any)["Send Payment"].firstMatch.tap()
             
-            app.descendants(matching:.any)["Confirm Payment"].tap()
+            app.descendants(matching:.any)["Confirm Payment"].firstMatch.tap()
         }
         step("I see a transaction") {
-            let transaction = app.descendants(matching:.any)["- € 13.37"]
+            let transaction = app.descendants(matching:.any)["- € 13.37"].firstMatch
             XCTAssertTrue(transaction.isHittable)
         }
         
